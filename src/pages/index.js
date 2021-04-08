6fbf7ae8e73bf749f6c1e03b9d55afd13934e5b1
23carnies <46233990+23carnies@users.noreply.github.com>
2021-04-08 10:59:27 -0400

    Update front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,17 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react';
+import Layout from '../components/layouts/layout';
+import About from '../components/sections/about';
+import Projects from '../components/sections/projects';
+import Contact from '../components/sections/contact';
+import Footer from '../components/layouts/footer';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
-
-export default IndexPage
+export default function Home() {
+  return (
+    <Layout>
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </Layout>
+  );
+}
