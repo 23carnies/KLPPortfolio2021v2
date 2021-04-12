ac8804f05e2ab5ebaaeefd17c468a1239d39934d
23carnies <46233990+23carnies@users.noreply.github.com>
2021-04-11 21:30:38 -0400

    Remove headline

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Flex, yellow3, imgBorder, title, text, below, white } from '../utilities'
+import { Flex, yellow3, title, text, below, white } from '../utilities'
+import { Section } from '../layouts/section'
 
 const About = () => {
 
     return ( 
-            <AboutBack>
-            <Headline>Hi! I'm Karen</Headline>
+            <Section>
             <Box>
                 <div style={{minWidth: '50%'}}>
                 <AboutText>From my earliest memories, I’ve always loved to learn. I taught myself to play guitar and built a car from scratch with my father as a teenager. I enjoy DIYing and cooking, both allowing my creative side to flourish. I’ve studied multiple human languages and I picked up HTML and CSS in college. For years I built sites for family and friends until deciding to pivot my career to software development. </AboutText>
@@ -14,25 +14,12 @@ const About = () => {
                 <AboutText>I have a passion for rowing, a sport of focus and dedication. I bring these skills with me to every project. With a fondness for spoken languages, travel, and fitness, I look to make a positive difference in the world. I want to see the world, and see what positive change I can be in it.</AboutText>
                 </div>
                 </Box>
-            </AboutBack>
+            </Section>
     );
 }
  
 export default About;
 
-
-const AboutBack = styled.div`
-    max-width: 1440px;
-    height: 100vh;
-    margin: 0 auto;
-    ${Flex({fd:'column'})};
-    border-bottom: 5px solid ${white};
-    ${below.xLarge`
-        max-width: 1280px;
-    `}
-    
-
-`;
 
 const Box = styled.div`
     background-image: url('/images/klo-pink-white-race.png');
@@ -40,19 +27,6 @@ const Box = styled.div`
     width: 100%;
     ${below.large`
         ${Flex({fd:'row',fw:'wrap'})};
-    `}
-`;
-
-const Headline = styled.h3`
-    text-align: center;
-    font-family: ${title};
-    color: ${yellow3};
-    margin: 1.5% 1% 1%;
-    ${below.large`
-        font-size: 2.4rem;
-    `}
-    ${below.medium`
-        font-size: 2rem;
     `}
 `;
 
