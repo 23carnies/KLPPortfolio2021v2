61d3ef0316b82cf304cc4eca24ad5deb272ebde9
23carnies <46233990+23carnies@users.noreply.github.com>
2021-04-11 21:32:26 -0400

    Add hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Layout from '../components/layouts/layout';
+import Hero from '../components/sections/hero';
 import About from '../components/sections/about';
 import Projects from '../components/sections/projects';
 import Contact from '../components/sections/contact';
-import Footer from '../components/layouts/footer';
 
 export default function Home() {
   return (
     <Layout>
+      <Hero />
       <About />
       <Projects />
       <Contact />
