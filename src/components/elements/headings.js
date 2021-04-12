eeb39bf8f6d3485bed90d8b3881006d4e3df6437
23carnies <46233990+23carnies@users.noreply.github.com>
2021-04-11 21:32:01 -0400

    Export headings

--- src/components/elements/headings.js
+++ src/components/elements/headings.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
-const Heading = styled.h1`
+export const Heading = styled.h1`
     color: black;
     font: bold 36px Helvetica, sans-serif;
 `;
 
-const SubHeading = styled.h2`
+export const SubHeading = styled.h2`
     color: black;
     font: bold 28px Helvetica, sans-serif;
+    text-align: center;
 `;
