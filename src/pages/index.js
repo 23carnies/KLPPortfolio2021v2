f298682f1b231fc6e5ed0806697423409c908ad9
23carnies <46233990+23carnies@users.noreply.github.com>
2021-04-12 15:51:45 -0400

    Add theme provider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/layouts/layout';
 import Hero from '../components/sections/hero';
 import About from '../components/sections/about';
 import Projects from '../components/sections/projects';
 import Contact from '../components/sections/contact';
+import { themes } from '../components/utilities'
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../../Global';
 
 export default function Home() {
+  const [theme, setTheme] = useState('light');
+
   return (
-    <Layout>
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
-    </Layout>
+    <ThemeProvider theme={themes[theme]}>
+      <GlobalStyle />
+      <Layout theme={theme} setTheme={setTheme}>
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+      </Layout>
+    </ThemeProvider>
+
   );
 }
