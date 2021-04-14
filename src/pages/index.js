95ae29691db94fa360b1d2513b0da04154917a08
23carnies <46233990+23carnies@users.noreply.github.com>
2021-04-14 08:38:46 -0400

    Add intro section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,19 @@ import Hero from '../components/sections/hero';
 import About from '../components/sections/about';
 import Projects from '../components/sections/projects';
 import Contact from '../components/sections/contact';
+import Intro from '../components/sections/intro';
 import { themes } from '../components/utilities'
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../../Global';
 
 export default function Home() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('dark');
 
   return (
     <ThemeProvider theme={themes[theme]}>
       <GlobalStyle />
       <Layout theme={theme} setTheme={setTheme}>
+        <Intro />
         <Hero />
         <About />
         <Projects />
