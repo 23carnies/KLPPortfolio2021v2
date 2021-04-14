41e670ec5740b9afb08232400de42b20ed57a06a
23carnies <46233990+23carnies@users.noreply.github.com>
2021-04-14 07:47:22 -0400

    Adjust text

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -11,7 +11,7 @@ const About = () => {
                 <div style={{minWidth: '50%'}}>
                 <AboutText>From my earliest memories, I’ve always loved to learn. I taught myself to play guitar and built a car from scratch with my father as a teenager. I enjoy DIYing and cooking, both allowing my creative side to flourish. I’ve studied multiple human languages and I picked up HTML and CSS in college. For years I built sites for family and friends until deciding to pivot my career to software development. </AboutText>
                 <AboutText>A recent graduate of the Software Engineering Immersive at General Assembly, I am experienced in modern web design. Skilled in pure CSS and several frameworks, I build alluring sites and apps with great functionality.</AboutText>
-                <AboutText>I have a passion for rowing, a sport of focus and dedication. I bring these skills with me to every project. With a fondness for spoken languages, travel, and fitness, I look to make a positive difference in the world. I want to see the world, and see what positive change I can be in it.</AboutText>
+                <AboutText>I have a passion for rowing, a sport of focus and dedication. I bring these skills with me to every project. With a fondness for spoken languages, travel, and fitness, I want to see the world; and see what positive change I can be in it.</AboutText>
                 </div>
                 </Box>
             </Section>
