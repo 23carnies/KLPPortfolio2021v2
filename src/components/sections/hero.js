783609dc328e267629c3d6c4b730670cce78497a
23carnies <46233990+23carnies@users.noreply.github.com>
2021-04-14 08:38:22 -0400

    Remove text

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import styled from 'styled-components'
 import { Section } from '../layouts/section'
-import { Heading, SubHeading } from '../elements'
 import Race from '../../images/klo-racing.png'
 
 const Hero = () => {
     return (
-        <Section id="hero" type="hero">
-            <Heading type="hero">Hi! I'm Karen</Heading>
-            <SubHeading type="hero">I am a software engineer based in Atlanta, focused on Front End design, useability, and accessability.</SubHeading>
+        <HeroSection id="hero">
             <Image src={Race} alt="Karen racing a rowing boat" />
-        </Section>
+        </HeroSection>
     );
 }
  
 export default Hero;
+
+const HeroSection = styled(Section)`
+    border-bottom: none;
+`;
 
 const Image = styled.img`
     width: 100%;
