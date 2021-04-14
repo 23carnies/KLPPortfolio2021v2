6624c025c06598be04d93c5867663c45ca9cf3fc
23carnies <46233990+23carnies@users.noreply.github.com>
2021-04-14 08:37:58 -0400

    Add border bottom

--- src/components/layouts/section.js
+++ src/components/layouts/section.js
@@ -1,12 +1,8 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 import { Flex } from '../utilities';
 
 export const Section = styled.section`
-    position: ${({type}) => type === 'hero'
-        ? 'relative'
-        : 'static'
-    };
-    background: blueviolet;
-    ${Flex({fd:'column',ai:'center',jc:'center'})}
-    /* margin: 0 auto; */
+  border-bottom: 10px solid ${props => props.theme.tagLineColor};
+  padding-bottom: 2%;
+  ${Flex({ fd: 'column', ai: 'center', jc: 'center' })};
 `;
