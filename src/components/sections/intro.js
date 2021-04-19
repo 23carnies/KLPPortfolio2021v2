e23599157dc27a08fb5f3e387b243b05bc49e3fb
23carnies <46233990+23carnies@users.noreply.github.com>
2021-04-19 17:42:16 -0400

    Adjust mobile view

--- src/components/sections/intro.js
+++ src/components/sections/intro.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Section } from '../layouts/section';
-import { Flex } from '../utilities';
+import { below, Flex } from '../utilities';
 import styled from 'styled-components';
 import Iceland from '../../images/klo-iceland.jpg';
 
@@ -25,22 +25,38 @@ const SectionIntro = styled(Section)`
   ${Flex};
   border-bottom: 10px solid ${props => props.theme.tagLineColor};
   padding-bottom: 2%;
+  ${below.small`
+    ${Flex({fd:'column'})};
+  `}
 `;
 
 const Image = styled.img`
   border-radius: 60%;
   width: 30%;
   margin: 1%;
+  border: 2px inset ${props => props.theme.pageBackground};
+  ${below.small`
+    width: 85%;
+    margin: 5% 0;
+  `}
 `;
 
 const TextBox = styled.div`
   width: 50%;
   padding: 2%;
+  ${below.small`
+    width: 90%;
+    padding: 0 1%;
+  `}
 `;
 
 const Heading = styled.h1`
   color: ${props => props.theme.titleColor};
   font: 700 36px 'Josefin Sans', sans-serif;
+  ${below.small`
+    line-height: 1rem;
+    margin: 1rem 0;
+  `}
 `;
 
 const SubHeading = styled.h2`
