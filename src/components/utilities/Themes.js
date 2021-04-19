f673898be211363dc4af9ea86fdc4fcc06356f18
23carnies <46233990+23carnies@users.noreply.github.com>
2021-04-19 19:23:10 -0400

    Update theme colors

--- src/components/utilities/Themes.js
+++ src/components/utilities/Themes.js
@@ -1,25 +1,25 @@
 export const darkTheme = {
   pageBackground: '#0b132b',
-  componentBackground: '#393e46',
-  titleColor: '#f2b002',
-  tagLineColor: '#007f97',
+  componentBackground: '#556272',
+  titleColor: '#BEE9E7',
+  tagLineColor: '#18809b',
   fontColor: '#ffffd9',
-  button1: '#050505',
-  button2: '#393e46',
-  gradient1: '#710b79',
-  gradient2: '#c901a1',
+  button1: '#FF6B6B',
+  button2: '#C54D57',
+  gradient1: '#BEE9E7',
+  gradient2: '#18809b',
 };
 
 export const lightTheme = {
-  pageBackground: '#b6ac9b',
-  componentBackground: '#b6ac9b',
-  titleColor: '#6b1914',
-  tagLineColor: '#d47006',
+  pageBackground: '#8e8e8e',
+  componentBackground: '#e3e3e3',
+  titleColor: '#6f5e3f',
+  tagLineColor: '#ef9a58',
   fontColor: '#051126',
-  button1: '#4c3f77',
-  button2: '#7c587f',
-  gradient1: '#4c3f77',
-  gradient2: '#7c587f',
+  button1: '#F6C7A2',
+  button2: '#ADA4B5',
+  gradient1: '#92709A',
+  gradient2: '#E29A9F',
 };
 
 export const themes = {
@@ -27,13 +27,3 @@ export const themes = {
   dark: darkTheme,
 };
 
-// export default {
-//     colors: {
-//       textDefault: '#4a4a4a',
-//       backgroundContent: '#f4f7fa',
-//       backgroundMenu: '#61a58b',
-//     },
-//     fontFamilies: {
-//       proxima: 'proxima-nova, sans-serif',
-//     },
-//   };
